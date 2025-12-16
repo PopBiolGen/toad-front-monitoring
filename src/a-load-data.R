37dfcc1852881f4ea7bb51480aa321b61131df42
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-12-16 16:10:24 +0800

    update time to detection pipeline and model

--- src/a-load-data.R
+++ src/a-load-data.R
@@ -8,17 +8,17 @@ df.fulcrum <- st_read(dsn = "dat/2025/2025_visual-surveys", layer = "toad_survey
   select(-(2:5), -photos, - audio, -X_latitude, -X_longitude) |>
   mutate(year = year(date),
          month = month(date),
-         day = day(date)
+         day = day(date),
+         temperature = as.numeric(temperatur),
+         water_available = "available",
+         survey_type = ifelse(how_many_p !=0, "nocturnal", "interview") 
   )
 
 #2023-4 visual survey data
 df.recon <- read_excel(path = "dat/invasion-front-reconnaissance-data.xlsx", sheet = "recon_data") |>
   mutate(hour = hour(time), 
          minute = minute(time), 
-         date = as.Date(date),
-         ttd = how_many_p*search_tim,
-         ttd.censored = ifelse(any_cane_t=="yes", ttd, NA), # only report ttd for positive sighting
-         censored = as.numeric(any_cane_t=="no")) |> # censored, or not?
+         date = as.Date(date)) |> 
   st_as_sf(coords = c("X_longitude", "X_latitude")) |>
   st_set_crs(value = 4283) |> # set crs (GDA94/GRS 1980)
   st_transform(crs = st_crs(df.fulcrum)) # transform to whatever comes from fulcrum
@@ -33,7 +33,12 @@ df.ns <- data.frame(X_longitude = X.ns, X_latitude = Y.ns, any_cane_t = "yes") |
   st_transform(crs = st_crs(df.fulcrum))
 
 # merge the datasets
-df <- bind_rows(df.fulcrum, df.recon, df.ns)
+df <- bind_rows(df.fulcrum, df.recon, df.ns) |> 
+  mutate(search_tim = ifelse(search_tim==0, 0.5, search_tim), #ensure even small times are positive
+         person.minutes = how_many_p*search_tim,
+         p.m.positive = ifelse(any_cane_t=="yes", person.minutes, NA), # only report person.minutes for positive sighting
+         censored = as.numeric(any_cane_t=="no"), # censored, or not?
+         toad.present = 1-censored) # toad present, or not 
 rm(df.recon, df.fulcrum, df.ns)
 
 # st_write(df.recon, "out/recon_sites.kml", append = FALSE)
--- src/time-to-detection_JAGS.R
+++ src/time-to-detection_JAGS.R
@@ -2,14 +2,16 @@ source("src/a-load-data.R")
 library(rjags)
 
 # select the relevant data
-in.dat <- filter(df.recon, water.status == "available" & survey.type == "nocturnal")
+in.dat <- filter(df, water_available == "available" & survey_type == "nocturnal")
 
 # make lists of data to feed JAGS
-data.list <- list(ttd = in.dat$ttd.censored, # time to detection data (NA's where toads not found)
-                  censored = in.dat$censored, 
+data.list <- list(ttd = in.dat$p.m.positive, # time to detection data (NA's where toads not found)
+                  is.detected = in.dat$toad.present, 
                   tmax.i = in.dat$person.minutes,
                   n.obs = nrow(in.dat))
-init.list <- list(p.occ = 0.2, lambda = 1/20, occ = in.dat$toad.present, ttd = ifelse(in.dat$toad.present==1, NA, in.dat$ttd.censored))
+init.list <- list(p.occ = 0.2, 
+                  lambda = 1/10, 
+                  occ = ifelse(data.list$is.detected == 1, 1, rbinom(length(data.list$is.detected), 1, 0.2)))
 
 # the model
 ttd.mod <- jags.model(file = "src/model-files/time-to-detection_JAGS.txt", 
@@ -24,5 +26,30 @@ ttd.samp<-coda.samples(ttd.mod,
                 thin = 5)
 gelman.diag(ttd.samp)
 
-summary(ttd.samp)
+(mod1 <- summary(ttd.samp))
 coda::densplot(ttd.samp)
+
+
+# plot cumulative probability of detection with time
+cum.exp <- function(t, lam){
+  1-exp(-lam*t)
+}
+time <- seq(0, 30, 0.2) # 0-30 minutes
+y.mean <- cum.exp(time, mod1$quantiles["lambda", "50%"])
+y.upp <- cum.exp(time, mod1$quantiles["lambda", "2.5%"])
+y.low <- cum.exp(time, mod1$quantiles["lambda", "97.5%"])
+
+pdat <- data.frame(Time = time, y.mean, y.low, y.upp) #|> 
+
+ggplot(data = pdat, aes(x = Time)) +
+    geom_ribbon(
+    aes(ymin = y.low,
+        ymax = y.upp),
+    fill = "lightblue", alpha = 0.3
+  ) +
+  geom_line(aes(y = y.mean)) +
+  labs(x = "Person minutes", y = "Detection probability")
+ggsave(filename = "out/detection-plot.pdf") 
+
+ggplot(data = in.dat) +
+  geom_histogram(aes(x = p.m.positive))
