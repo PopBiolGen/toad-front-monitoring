461784f6e39c864172d0d07b11ab7288bebea319
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-12-19 07:36:04 +0800

    draft invasion occupancy model running

--- src/time-to-detection-invasion_JAGS.R
+++ src/time-to-detection-invasion_JAGS.R
@@ -4,24 +4,41 @@ library(rjags)
 # select the relevant data
 in.dat <- filter(df, water_available == "available" & survey_type == "nocturnal")
 
+# get x, y coordinates for each record, in metres and add to dataframe
+proj.coords <- in.dat |> 
+  st_transform(crs = 3577) |> # project to albers
+  st_coordinates()
+
+mean.coord <- colMeans(proj.coords) # make centre of study site the reference point
+proj.coords.centred <- sweep(proj.coords, 2, mean.coord, FUN = "-") |>
+  as.data.frame() |> 
+  rename(X.c = X, Y.c = Y)
+
+in.dat <- cbind(in.dat, proj.coords, proj.coords.centred)
+
 # make lists of data to feed JAGS
 data.list <- list(ttd = in.dat$p.m.positive, # time to detection data (NA's where toads not found)
                   is.detected = in.dat$toad.present, 
                   tmax.i = in.dat$person.minutes,
-                  n.obs = nrow(in.dat))
-init.list <- list(p.occ = 0.2, 
-                  lambda = 1/10, 
+                  n.obs = nrow(in.dat),
+                  x = in.dat$X.c,
+                  y = in.dat$Y.c,
+                  t = in.dat$year-min(in.dat$year))
+init.list <- list(lambda = 1/10, 
                   occ = ifelse(data.list$is.detected == 1, 1, rbinom(length(data.list$is.detected), 1, 0.2)))
 
 # the model
-ttd.mod <- jags.model(file = "src/model-files/time-to-detection_JAGS.txt", 
+ttd.mod <- jags.model(file = "src/model-files/time-to-detection-invasion_JAGS.txt", 
            data = data.list, 
            inits = init.list,
            n.chains = 3)
 update(ttd.mod, n.iter = 5000) # burn in
 ttd.samp<-coda.samples(ttd.mod, 
                 variable.names = c("lambda",
-                                   "p.occ"), 
+                                   "beta",
+                                   "a",
+                                   "b",
+                                   "c"), 
                 n.iter = 10000, 
                 thin = 5)
 gelman.diag(ttd.samp)
